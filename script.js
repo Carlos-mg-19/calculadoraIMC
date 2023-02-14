a29f425ea25f677c44b249668645a153501e88b1
Carlos-mg-19 <115847662+Carlos-mg-19@users.noreply.github.com>
2023-02-14 18:15:06 -0300

    Update script.js

--- script.js
+++ script.js
@@ -12,24 +12,21 @@ function calcular(){
         let N = nome.value
         let res = ''
         let IMC = (P / (A * A))
-        if( IMC < 18,5){
+        if( IMC < 18.5){
             res = 'Abaixo do peso normal'
-        }
-        if(IMC < 24,9){
+        }else if (IMC < 25){
             res = 'peso normal'
-        }
-        if(IMC > 24,9 || IMC < 29,9){
-            res = 'excesso de peso'
-        }
-        if(IMC > 29,9 || IMC < 34,9){
-            res = 'obesidade classe I'
-        }
-        if(IMC > 34,9 || IMC < 39,9){
-            res = 'obesidade classe II'
-        }
-        if(IMC >= 40,0){
-            res = 'obesidade classe III'
-        }
+        }else if (IMC < 30){
+            res = 'Acima do peso'
+            }else if (IMC < 35){
+                res = 'obesidade grau I'
+            }else if (IMC < 40){
+                res = 'obesidade grau II'
+            }else {
+                res = 'obesidade grau III'
+            }
+        
+  
 
         resutado.innerHTML = (` ${N} seu IMC é de ${IMC.toFixed(1)} ${res}`)
     }
